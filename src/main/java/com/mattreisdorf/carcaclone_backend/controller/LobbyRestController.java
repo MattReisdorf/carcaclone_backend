57e5e88be04580813f64be31f8687374b0cd7671
Matt Reisdorf <mreisdorf9717@gmail.com>
2025-05-07 16:07:37 -0400

    rest endpoints for getting all active lobbies and a specific active lobby

---
+++ src/main/java/com/mattreisdorf/carcaclone_backend/controller/LobbyRestController.java
@@ -0,0 +1,40 @@
+package com.mattreisdorf.carcaclone_backend.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mattreisdorf.carcaclone_backend.model.Lobby;
+import com.mattreisdorf.carcaclone_backend.service.LobbyManager;
+
+@RestController
+@RequestMapping("/api/lobbies")
+public class LobbyRestController {
+  
+  @Autowired
+  private LobbyManager lobbyManager;
+
+  // Endpoint for listing all active public lobbies
+  @GetMapping
+  public Collection<Lobby> getActiveLobbies() {
+    Collection<Lobby> lobbies = lobbyManager.getAllLobbies();
+    return (lobbies == null) ? new ArrayList<>() : lobbies;
+  }
+
+  // Endpoint for ensuring lobby existence when attempting to join lobby
+  @GetMapping("/{lobbyId}")
+  public ResponseEntity<Lobby> getLobby(@PathVariable String lobbyId) {
+    Lobby lobby = lobbyManager.getLobby(lobbyId);
+    if (lobby == null) {
+      return ResponseEntity.notFound().build();
+    }
+    System.out.println(lobby.toString());
+    return ResponseEntity.ok(lobby);
+  }
+}
