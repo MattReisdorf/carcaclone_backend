567a3823db993e5a61cebcd445dbfb97d8900519
Matt Reisdorf <mreisdorf9717@gmail.com>
2025-05-08 00:12:13 -0400

    endpoints for ready and lobby privacy status; converted to messagingTemplate and Principal for createLobby endpoint -> messages should go back to single user only, prevents anyone on that page at lobby creation time from being rerouted; other lobby endpoints use messagingTemplate that sends to topic/lobby/{lobbyId} so only the publishing lobby gets updates

--- src/main/java/com/mattreisdorf/carcaclone_backend/controller/LobbyController.java
+++ src/main/java/com/mattreisdorf/carcaclone_backend/controller/LobbyController.java
@@ -1,11 +1,16 @@
 package com.mattreisdorf.carcaclone_backend.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.mattreisdorf.carcaclone_backend.dto.lobby_messages.CreateLobbyMessage;
+import com.mattreisdorf.carcaclone_backend.dto.lobby_messages.JoinLobbyMessage;
+import com.mattreisdorf.carcaclone_backend.dto.lobby_messages.PlayerReadyMessage;
+import com.mattreisdorf.carcaclone_backend.dto.lobby_messages.PrivateLobbyMessage;
 import com.mattreisdorf.carcaclone_backend.model.Lobby;
 import com.mattreisdorf.carcaclone_backend.service.LobbyManager;
 
@@ -14,11 +19,54 @@ public class LobbyController {
 
   @Autowired
   private LobbyManager lobbyManager;
-  
+
+  @Autowired
+  private SimpMessagingTemplate messagingTemplate;
+
+  // Create a new lobby
   @MessageMapping("/createLobby")
-  @SendTo("/topic/lobby")
-  public Lobby createLobby(CreateLobbyMessage message) {
+  public void createLobby(CreateLobbyMessage message, Principal principal) {
     Lobby lobby = lobbyManager.createLobby(message.getPlayerId(), message.getPlayerName());
-    return lobby;
+    if (lobby == null) {
+      return;
+    }
+    messagingTemplate.convertAndSendToUser(
+        principal.getName(),
+        "/queue/lobbyCreated",
+        lobby);
+  }
+
+  // Join an existing lobby
+  @MessageMapping("/joinLobby")
+  public void joinLobby(JoinLobbyMessage message) {
+    Lobby lobby = lobbyManager.joinLobby(message.getLobbyId(), message.getPlayerId(), message.getPlayerName());
+    if (lobby == null) {
+      return;
+    }
+    messagingTemplate.convertAndSend(
+        "/topic/lobby/" + message.getLobbyId(),
+        lobby);
+  }
+
+  @MessageMapping("/ready")
+  public void markReady(PlayerReadyMessage message) {
+    Lobby lobby = lobbyManager.setPlayerReady(message.getPlayerId(), message.getLobbyId(), message.isPlayerReady());
+    if (lobby == null) {
+      return;
+    }
+    messagingTemplate.convertAndSend(
+        "/topic/lobby/" + message.getLobbyId(),
+        lobby);
+  }
+
+  @MessageMapping("/setPrivate")
+  public void setPrivate(PrivateLobbyMessage message) {
+    Lobby lobby = lobbyManager.setLobbyPrivate(message.getLobbyId(), message.getIsPrivateLobby());
+    if (lobby == null) {
+      return;
+    }
+    messagingTemplate.convertAndSend(
+        "/topic/lobby/" + message.getLobbyId(),
+        lobby);
   }
 }
