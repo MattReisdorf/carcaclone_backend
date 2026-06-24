2275b8bb05e7b60756bd8a015525ff669cf4af45
Matt Reisdorf <mreisdorf9717@gmail.com>
2026-06-24 14:23:57 -0400

    Implement HTTP Session Handshake Interceptor; Starts user session on valid connection

--- src/main/java/com/mattreisdorf/carcaclone_backend/config/WebSocketConfig.java
+++ src/main/java/com/mattreisdorf/carcaclone_backend/config/WebSocketConfig.java
@@ -5,17 +5,26 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import com.mattreisdorf.carcaclone_backend.dto.PlayerSessionResponse;
+import com.mattreisdorf.carcaclone_backend.service.PlayerSessionService;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final PlayerSessionService playerSessionService;
+
+  public WebSocketConfig(PlayerSessionService playerSessionService) {
+    this.playerSessionService = playerSessionService;
+  }
 
   @Override
   public void configureMessageBroker(@SuppressWarnings("null") MessageBrokerRegistry registry) {
@@ -29,18 +38,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     registry
         .addEndpoint("/ws")
         .setAllowedOrigins("http://localhost:5173") // or use allowedOriginPatterns
+        .addInterceptors(new HttpSessionHandshakeInterceptor())
         .setHandshakeHandler(new DefaultHandshakeHandler() {
           @Override
           protected Principal determineUser(@SuppressWarnings("null") ServerHttpRequest request,
               @SuppressWarnings("null") WebSocketHandler wsHandler,
               @SuppressWarnings("null") Map<String, Object> attributes) {
-            if (request instanceof ServletServerHttpRequest servletReq) {
-              String playerId = servletReq.getServletRequest().getParameter("playerId");
-              if (playerId != null && !playerId.isBlank()) {
-                return new StompPrincipal(playerId);
-              }
+            try {
+              PlayerSessionResponse player = playerSessionService.getPlayerFromAttributes(attributes);
+              return new StompPrincipal(player.playerId(), player.playerName());
+            } catch (IllegalStateException exception) {
+              return super.determineUser(request, wsHandler, attributes);
             }
-            return super.determineUser(request, wsHandler, attributes);
           }
         })
         .withSockJS();
